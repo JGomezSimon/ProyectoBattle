2b2ec418941ed7da415bf4f27ee732fd9fb7d3c2
Jaume Gomez Simon <jgomezsimon13@gmail.com>
2021-05-10 13:22:53 +0200

    BBDD V.2

--- m2/battle.sql
+++ m2/battle.sql
@@ -1,4 +1,4 @@
-DROP DATABASE `battle`;
+DROP DATABASE IF EXISTS `battle`;
 
 SET SQL_MODE=NO_AUTO_VALUE_ON_ZERO;
 
@@ -6,35 +6,43 @@ CREATE DATABASE `battle`;
 USE `battle`;
 
 CREATE TABLE weapons(
-	weapon_id int(10) AUTO_INCREMENT primary key,
+	weapon_id int AUTO_INCREMENT primary key,
     weapon_name varchar(255),
     weapon_image_path varchar(255),
-    weapon_strength_boost int(10),
-    weapon_speed_boost int(10),
+    weapon_strength_boost int,
+    weapon_speed_boost int,
     dwarf_use boolean,
     elf_use boolean,
     human_use boolean
 );
 
+CREATE TABLE races(
+	race_id int auto_increment primary key,
+    race_name varchar(255),
+	health_points int,
+    strength int,
+    defense int,
+    agility int,
+    speed int
+);
+
 CREATE TABLE warriors(
-	warrior_id int(10) auto_increment primary key,
+	warrior_id int auto_increment primary key,
     warrior_name varchar(255),
     warrior_image_path varchar(255),
-    race varchar(255),
-	health_points int(10),
-    strength int(10),
-    defense int(10),
-    agility int(10),
-    speed int(10)
+    race_id int,
+    CONSTRAINT Wrace_id FOREIGN KEY (race_id)
+		REFERENCES races(race_id)
 );
 
 CREATE TABLE players(
-	player_id int(10) auto_increment primary key,
+	player_id int auto_increment primary key,
     player_name varchar(255),
-    global_points int(10),
-    enemies_defeated int(10),
-    damage_dealt int(10),
-    damage_recieved int(10)
+    global_points int,
+    enemies_defeated int,
+    damage_dealt int,
+    damage_recieved int,
+    warrior_id int
 );
 
 CREATE TABLE weapons_available(
@@ -47,25 +55,15 @@ CREATE TABLE weapons_available(
 );
 
 CREATE TABLE battles(
-	battle_id int(10) auto_increment primary key,
-    player_id int(10),
-    warrior_id int (10),
-    warrior_weapon_id int(10),
-    oponent_id int(10),
-    oponent_weapon_id int(10),
-    injuries_caused int(10),
-    injuries_suffered int(10),
-    battle_points int(10),
-    CONSTRAINT FK_Bplayer_id FOREIGN KEY (player_id)
-		REFERENCES players(player_id),
-    CONSTRAINT FK_BPwarrior FOREIGN KEY (warrior_id)
-        REFERENCES warriors(warrior_id),
-	CONSTRAINT FK_BPweapon FOREIGN KEY (warrior_weapon_id)
-        REFERENCES weapons(weapon_id),
-	CONSTRAINT FK_BOwarrior FOREIGN KEY (oponent_id)
-        REFERENCES warriors(warrior_id),
-	CONSTRAINT FK_BOweapon FOREIGN KEY (oponent_weapon_id)
-        REFERENCES weapons(weapon_id)
+	battle_id int auto_increment primary key,
+    player_id int,
+    warrior_id int,
+    warrior_weapon_id int,
+    oponent_id int,
+    oponent_weapon_id int,
+    injuries_caused int,
+    injuries_suffered int,
+    battle_points int
 );
 
 insert into weapons(weapon_id,weapon_name,weapon_image_path,weapon_strength_boost,weapon_speed_boost,dwarf_use,elf_use,human_use) values 
@@ -79,16 +77,21 @@ insert into weapons(weapon_id,weapon_name,weapon_image_path,weapon_strength_boos
 (8,'Knife','ejemplo.jpg',0,4,true,true,true),
 (9,'Two-Handed Axe','ejemplo.jpg',5,0,true,false,false);
 
-insert into warriors(warrior_id,warrior_name,warrior_image_path,race,health_points,strength,defense,agility,speed) values
-(1,'Nedraec Forgeshaper','ejemplo.jpg','dwarf',60,6,4,5,3),
-(2,'Safomli Platebreaker','ejemplo.jpg','dwarf',60,6,4,5,3),
-(3,'Khekroc Heavyshoulder','ejemplo.jpg','dwarf',60,6,4,5,3),
-(4,'Edwaradrin Ohmswis','ejemplo.jpg','elf',40,4,2,7,7),
-(5,'Legomoreno','ejemplo.jpg','elf',40,4,2,7,7),
-(6,'Rogerolas Elfbrooks','ejemplo.jpg','elf',40,4,2,7,7),
-(3,'Maol-Chaluim Maoileanach','ejemplo.jpg','human',50,5,3,6,5),
-(3,'Gillìosa Fòlais','ejemplo.jpg','human',60,5,3,6,5),
-(3,'Seumas Forsàidh','ejemplo.jpg','human',60,5,4,6,5)
+insert into races(race_id, race_name,health_points,strength,defense,agility,speed) values 
+(1,'Dwarf',60,6,4,5,3),
+(2,'Elf',40,4,2,7,7),
+(3,'Human',50,5,4,6,5);
+
+insert into warriors(warrior_id,warrior_name,warrior_image_path,race_id) values
+(1,'Nedraec Forgeshaper','ejemplo.jpg',1),
+(2,'Safomli Platebreaker','ejemplo.jpg',1),
+(3,'Khekroc Heavyshoulder','ejemplo.jpg',1),
+(4,'Edwaradrin Ohmswis','ejemplo.jpg',2),
+(5,'Legomoreno','ejemplo.jpg',2),
+(6,'Rogerolas Elfbrooks','ejemplo.jpg',2),
+(7,'Maol-Chaluim Maoileanach','ejemplo.jpg',3),
+(8,'Gillìosa Fòlais','ejemplo.jpg',3),
+(9,'Seumas Forsàidh','ejemplo.jpg',3)
 
 
 
