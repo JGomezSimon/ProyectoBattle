8d43556fc89ae66a6cd0dfdb4396cbbb53e9a4dd
Jaume Gomez Simon <jgomezsimon13@gmail.com>
2021-05-18 10:58:59 +0200

    URL BBDD actualizada

--- m2/battle.sql
+++ m2/battle.sql
@@ -61,30 +61,27 @@ CREATE TABLE battles(
 );
 
 insert into weapons(weapon_id, weapon_name, weapon_image_path, weapon_strength_boost, weapon_speed_boost, weapon_points) values 
-(1, 'Dagger', 'assets/weapons/dagger.jpg', 0, 3, 10),
-(2, 'Sword', 'assets/weapons/sword.jpg', 1, 1, 10),
-(3, 'Axe', 'assets/weapons/axe.png', 3, 0, 10),
-(4, 'Twin Swords', 'assets/weapons/twinSwords.jpg',2 ,2 ,14),
-(5, 'Scimitar', 'assets/weapons/scimitar.jpg',1 ,2 , 14),
-(6, 'Bow', 'assets/weapons/bow.jpg', 1, 5, 15),
-(7, 'Katana', 'assets/weapons/katana.jpg', 2, 3, 18),
-(8, 'Knife', 'assets/weapons/knife.jpg', 0, 4, 12),
-(9, 'Two-Handed Axe', 'assets/weapons/twoHandedAxe.jpg', 5, 0, 20);
+(1, 'Dagger', 'src/main/resources/weapons/dagger.jpg', 0, 3, 10),
+(2, 'Sword', 'src/main/resources/weapons/sword.jpg', 1, 1, 10),
+(3, 'Axe', 'src/main/resources/weapons/axe.png', 3, 0, 10),
+(4, 'Twin Swords', 'src/main/resources/weapons/twinSwords.jpg',2 ,2 ,14),
+(5, 'Scimitar', 'src/main/resources/weapons/scimitar.jpg',1 ,2 , 14),
+(6, 'Bow', 'src/main/resources/weapons/bow.jpg', 1, 5, 15),
+(7, 'Katana', 'src/main/resources/weapons/katana.jpg', 2, 3, 18),
+(8, 'Knife', 'src/main/resources/weapons/knife.jpg', 0, 4, 12),
+(9, 'Two-Handed Axe', 'src/main/resources/weapons/twoHandedAxe.jpg', 5, 0, 20);
 
 
 
 insert into warriors(warrior_id, warrior_name, warrior_image_path, race_id, health_points, strength, defense, agility, speed, race_points) values
-(1, 'Nedraec Forgeshaper', 'assets/warriors/nedraec.jpeg', 1, 60, 6, 4, 5, 3, 21),
-(2, 'Safomli Platebreaker', 'assets/warriors/samfoli.jpg', 1, 60, 6, 4, 5, 3, 21),
-(3, 'Khekroc Heavyshoulder', 'assets/warriors/khekroc.png', 1, 60, 6, 4, 5, 3, 21),
-(4, 'Edwaradrin Ohmswis', 'assets/warriors/edwaradrin.jpg', 2, 40, 4, 2, 7, 7, 19),
-(5, 'Legomoreno', 'assets/warriors/legomoreno.jpg', 2, 40, 4, 2, 7, 7, 19),
-(6, 'Rogerolas Elfbrooks', 'assets/warriors/roferolas.jpg', 2, 40, 4, 2, 7, 7, 19),
-(7, 'Maol-Chaluim Maoileanach', 'assets/warriors/maol-chaluim.jpg', 3, 50, 5, 4, 6, 5, 20),
-(8, 'Gillìosa Fòlais', 'assets/warriors/gilliosa.jpg', 3, 50, 5, 4, 6, 5, 20),
-(9, 'Seumas Forsàidh', 'assets/warriors/seumas.jpg', 3, 50, 5, 4, 6, 5, 20);
+(1, 'Nedraec Forgeshaper', 'src/main/resources/warriors/nedraec.jpeg', 1, 60, 6, 4, 5, 3, 21),
+(2, 'Safomli Platebreaker', 'src/main/resources/warriors/samfoli.jpg', 1, 60, 6, 4, 5, 3, 21),
+(3, 'Khekroc Heavyshoulder', 'src/main/resources/warriors/khekroc.png', 1, 60, 6, 4, 5, 3, 21),
+(4, 'Edwaradrin Ohmswis', 'src/main/resources/warriors/edwaradrin.jpg', 2, 40, 4, 2, 7, 7, 19),
+(5, 'Legomoreno', 'src/main/resources/warriors/legomoreno.jpg', 2, 40, 4, 2, 7, 7, 19),
+(6, 'Rogerolas Elfbrooks', 'src/main/resources/warriors/roferolas.jpg', 2, 40, 4, 2, 7, 7, 19),
+(7, 'Maol-Chaluim Maoileanach', 'src/main/resources/warriors/maol-chaluim.jpg', 3, 50, 5, 4, 6, 5, 20),
+(8, 'Gillìosa Fòlais', 'src/main/resources/warriors/gilliosa.jpg', 3, 50, 5, 4, 6, 5, 20),
+(9, 'Seumas Forsàidh', 'src/main/resources/warriors/seumas.jpg', 3, 50, 5, 4, 6, 5, 20);
 
 insert into players(player_id, player_name, passwd, points, wins, losses, warrior_id) values (1,'test','test', 176, 7, 3, 5);
-
-
-
